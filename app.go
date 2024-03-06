3cc4da2dcc52bddca299fe92c14efc8410954795
Endg4meZer0 <timka.tarasen@gmail.com>
2024-03-06 17:31:44 +1000

    works (v0.1)

--- app.go
+++ app.go
@@ -1,30 +1,104 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/hugolgst/rich-go/client"
 	_ "github.com/joho/godotenv/autoload"
 )
 
+type ProcessToWatch struct {
+	ProcessName    string
+	Details        string
+	State          string
+	LargeImageKey  string
+	LargeImageText string
+	SmallImageKey  string
+	SmallImageText string
+	UseTimestamp   bool
+	Buttons        []ActivityButtons
+}
+
+type ActivityButtons struct {
+	Text string
+	Url  string
+}
+
+func SetActivity(currentHighestIndex *int, processes []ProcessToWatch) {
+	for i := range len(processes) {
+		out, err := exec.Command("tasklist", "/fi", fmt.Sprintf(`IMAGENAME eq %v`, processes[i].ProcessName)).Output()
+		if err != nil {
+			fmt.Printf("Warning: couldn't use the command \"tasklist\" for the process with the name of %v", processes[i].ProcessName)
+			continue
+		}
+		if string(out[:5]) == "INFO:" {
+			continue
+		} else if *currentHighestIndex > i {
+			*currentHighestIndex = i
+			break
+		}
+	}
+
+	processInfo := processes[*currentHighestIndex]
+	newActivity := client.Activity{
+		Details:    processInfo.Details,
+		State:      processInfo.State,
+		LargeImage: processInfo.LargeImageKey,
+		LargeText:  processInfo.LargeImageText,
+		SmallImage: processInfo.SmallImageText,
+		SmallText:  processInfo.SmallImageText,
+		Buttons:    []*client.Button{},
+	}
+	for i := range len(processInfo.Buttons) {
+		newActivity.Buttons = append(newActivity.Buttons, &client.Button{
+			Label: processInfo.Buttons[i].Text,
+			Url:   processInfo.Buttons[i].Url,
+		})
+	}
+	if processInfo.UseTimestamp {
+		now := time.Now()
+		newActivity.Timestamps = &client.Timestamps{
+			Start: &now,
+		}
+	}
+
+	err := client.SetActivity(newActivity)
+
+	if err != nil {
+		log.Panicln("Error: couldn't set the new activity.")
+	}
+}
+
 func main() {
-	err := client.Login(os.Getenv("CLIENT_ID"))
+	processesListFile, err := os.ReadFile("./processList.json")
+	if err != nil {
+		log.Panicln("Error: the file of processes to watch does not exist. Please, create processList.json in app folder.")
+	}
+
+	var processes []ProcessToWatch
+
+	err = json.Unmarshal(processesListFile, &processes)
+	if err != nil {
+		log.Panicln("Error: couldn't parse processList.json. Maybe it is not using proper JSON format?")
+	}
+	currentHighestIndex := len(processes) + 1
+
+	err = client.Login(os.Getenv("CLIENT_ID"))
 	fmt.Println("hello world")
 
 	if err != nil {
-		log.Fatal("Error while connecting. Check the client id in your .env file")
+		log.Panicln("Error while connecting. Check the client id in your .env file")
 	}
 
-	err = client.SetActivity(client.Activity{
-		State:   "TEST!!!!",
-		Details: "go test",
-	})
+	SetActivity(&currentHighestIndex, processes)
 
-	if err != nil {
-		log.Fatal("Error while setting an activity.")
+	ticker := time.NewTicker(30 * time.Second)
+	for range ticker.C {
+		SetActivity(&currentHighestIndex, processes)
 	}
-
-	fmt.Scanln()
 }
